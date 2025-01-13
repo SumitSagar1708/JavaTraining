d44f3b2fefff8ef75eb70614d0dbde98b936f7f0
SumitSagar1708 <sumit.sagar.108@gmail.com>
2025-01-13 21:36:26 +0530

    Assignmet12

--- src/com/creatio/crm/language/basics/Assignment12.java
+++ src/com/creatio/crm/language/basics/Assignment12.java
@@ -3,22 +3,31 @@ package com.creatio.crm.language.basics;
 public class Assignment12 {
 
 	public static void main(String[] args) {
-        int rows = 5; 
+		int rows = 5;
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
+		for (int i = 1; i <= rows; i++) {
 
-        
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-    }
+			for (int space = 1; space <= rows - i; space++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+		for (int i = rows - 1; i >= 1; i--) {
+
+			for (int space = 1; space <= rows - i; space++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
 
 }
