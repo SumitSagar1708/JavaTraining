84180a57fc83d6955000bb2a3a06becb815aeed8
SumitSagar1708 <sumit.sagar.108@gmail.com>
2025-01-13 22:59:33 +0530

    Assignmet12

--- src/com/creatio/crm/language/basics/Assignment12.java
+++ src/com/creatio/crm/language/basics/Assignment12.java
@@ -10,33 +10,31 @@ package com.creatio.crm.language.basics;
 public class Assignment12 {
 
 	public static void main(String[] args) {
-        int n = 5; 
+		int n = 5;
 
-        
-        for (int i = 1; i <= n; i++) {
-            // Print spaces for alignment
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // Print asterisks
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+		for (int i = 1; i <= n; i++) {
 
-        
-        for (int i = n - 1; i >= 1; i--) {
-            
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		for (int i = n - 1; i >= 1; i--) {
+
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 
 }
