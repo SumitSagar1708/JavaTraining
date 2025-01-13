ccad72f50d5d307d259314832855a034db0736b2
SumitSagar1708 <sumit.sagar.108@gmail.com>
2025-01-13 22:53:51 +0530

    Assignmet12

--- src/com/creatio/crm/language/basics/Assignment12.java
+++ src/com/creatio/crm/language/basics/Assignment12.java
@@ -1,8 +1,8 @@
 package com.creatio.crm.language.basics;
 
 /**
- * Assignment 12: Arrays and Loops
- * Print the numbers in diamond-shaped pyramid of numbers.
+ * Assignment 2: Product Data Storage with 3D Array
+ * Using a 3D Array.
  * Author:SumitSagar
  * Version: 1.0.0
  */
@@ -10,31 +10,33 @@ package com.creatio.crm.language.basics;
 public class Assignment12 {
 
 	public static void main(String[] args) {
-		int rows = 5;
+        int n = 5; 
 
-		for (int i = 1; i <= rows; i++) {
+        
+        for (int i = 1; i <= n; i++) {
+            // Print spaces for alignment
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
-			for (int space = 1; space <= rows - i; space++) {
-				System.out.print(" ");
-			}
-
-			for (int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
-
-		for (int i = rows - 1; i >= 1; i--) {
-
-			for (int space = 1; space <= rows - i; space++) {
-				System.out.print(" ");
-			}
-
-			for (int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
-	}
+        
+        for (int i = n - 1; i >= 1; i--) {
+            
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 
 }
